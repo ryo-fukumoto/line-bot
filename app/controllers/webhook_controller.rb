23299b519c75efeed9eec488b146a1e0a696cf59
ryo-fukumoto <gkrpw6767@gmail.com>
2019-08-20 19:37:27 +0900

    Update webhook_controller.rb

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -29,22 +29,39 @@ class WebhookController < ApplicationController
 
       #wikipediaから情報を取得する
       page = Wikipedia.find(word)
+      if page != nil
 
-      #内容とURLを返す
-      response = page.summary + "\n" + page.fullurl
+        #内容とURLを返す
+        response = page.summary + "\n" + page.fullurl
 
-      case event
-        #メッセージが送信された場合
-      when Line::Bot::Event::Message
-        case event.type
+        case event
+          #メッセージが送信された場合
+        when Line::Bot::Event::Message
+          case event.type
 
-          #メッセージが送られてきた場合
-        when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: response
-          }
-          client.reply_message(event['replyToken'], message)
+            #メッセージが送られてきた場合
+          when Line::Bot::Event::MessageType::Text
+            message = {
+              type: 'text',
+              text: response
+            }
+            client.reply_message(event['replyToken'], message)
+          end
+        end
+      else
+        case event
+          #メッセージが送信された場合
+        when Line::Bot::Event::Message
+          case event.type
+
+            #メッセージが送られてきた場合
+          when Line::Bot::Event::MessageType::Text
+            nomessage = {
+              type: 'text',
+              text: "無いよ"
+            }
+            client.reply_message(event['replyToken'], nomessage)
+          end
         end
       end
     }
