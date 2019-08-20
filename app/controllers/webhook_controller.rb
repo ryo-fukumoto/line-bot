78274d166dc1f4f2f12a3d5bbc72e52e83a54266
ryo-fukumoto <gkrpw6767@gmail.com>
2019-08-20 21:15:31 +0900

    Update webhook_controller.rb

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -52,12 +52,5 @@ class WebhookController < ApplicationController
     head :ok
   end
 
-  private
-
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
-  end
+ 
 end
